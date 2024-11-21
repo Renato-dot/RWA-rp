a23ab6262c5033b5a843d95c39aec247e14a1c40
Renato-dot <renatoprebegfax@gmail.com>
2024-11-21 10:57:30 +0100

    Backend

---
+++ backend/indeks.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+
+// Parser za JSON podatke
+app.use(bodyParser.json());
+
+// Parser za podatke iz formi
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: host
+    user: user,
+    password: password,
+    database: database
+  });
+  
+app.use(express.urlencoded({ extended: true }));
+  
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  app.listen(port, () => {
+    console.log("Server running at port: " + port);
+});
+
+app.get("/api/knjige", (req, res) => {
+    connection.query("SELECT * FROM knjige", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+  app.post("/api/rezerv_knjige", (req, res) => {
+    const data = req.body;
+    rezervacija = [[date.today, data.id_knjiga, data.id_korisnik]]
+    connection.query("INSERT INTO rezervacija (datum, knjiga, korisnik) VALUES ?", [rezervacija], (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
